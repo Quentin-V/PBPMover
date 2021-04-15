9097eaf0c2c53dadc99bcb77cbc03cd0094860f6
Quentin V <quentin.vauthier000@gmail.com>
2021-04-15 22:07:19 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace PBPMover
+{
+    partial class Fenetre
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.lblX = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(108, 16);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(100, 23);
+            this.tbX.TabIndex = 0;
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(108, 49);
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(100, 23);
+            this.tbY.TabIndex = 1;
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(36, 19);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(66, 15);
+            this.lblX.TabIndex = 2;
+            this.lblX.Text = "X position :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y position :";
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(49, 78);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(150, 37);
+            this.btnMove.TabIndex = 4;
+            this.btnMove.Text = "Move the window";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.brnMoveClick);
+            // 
+            // Fenetre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 127);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.tbX);
+            this.Name = "Fenetre";
+            this.Text = "PBP Mover";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbX;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnMove;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBPMover
+{
+    public partial class Fenetre : Form
+    {
+        public Fenetre()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void brnMoveClick(object sender, EventArgs e)
+        {
+            WindowScrape.Types.HwndObject pbpWindow = WindowScrape.Types.HwndObject.GetWindowByTitle("Penumbra");
+            String sX = tbX.Text;
+            String sY = tbY.Text;
+            int x = Int16.Parse(sX) - 3;
+            int y = Int16.Parse(sY) - 26;
+            pbpWindow.Location = new Point(x, y);
+        }
+    }
+}
